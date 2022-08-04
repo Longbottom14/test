f05471908fd6d93d58f8fe7a2cdd95b495166d83
Ridwan Abdulkareem <ridwan.abdkareem@gmail.com>
2022-08-04 17:48:54 +0100

    water

--- hungry.py
+++ hungry.py
@@ -3,4 +3,7 @@ hungry = input("Are you hungry")
 if hungry == 'yes':
     print('eat samosa')
 else:
-    print('code')
+    #print('code')
+    thirsty = input("Are you thirsty : ")
+    if thirsty == 'yes':
+        print('Drink water')
